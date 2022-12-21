a8673cc85319cdc452da76340134759921b585ad
KirSupi <kirsupi@gmail.com>
2022-12-21 11:39:17 +0300

    mvp

--- herder.go
+++ herder.go
@@ -1,24 +1,27 @@
-package herder
+package repository
 
 import (
+	"bytes"
 	"errors"
-	"go-herder/internal/repository"
+	"fmt"
+	"strconv"
 	"sync"
+	"text/tabwriter"
 )
 
-type Config struct {
+type HerderConfig struct {
 }
 
 type Herder struct {
 	Processes []*Process
-	c         *Config
-	r         repository.Repository
+	c         *HerderConfig
+	r         Repository
 	m         *sync.Mutex
 }
 
-func New(c Config, r repository.Repository) *Herder {
+func NewHerder(c *HerderConfig, r Repository) *Herder {
 	return &Herder{
-		c: &c,
+		c: c,
 		r: r,
 		m: &sync.Mutex{},
 	}
@@ -134,4 +137,39 @@ func (h *Herder) CheckProcessExists(processID int) error {
 		}
 	}
 	return errorNoProcessID(processID)
+}
+
+func StringifyStates(states []ProcessState) string {
+	needToPrintLabel := false
+	for _, s := range states {
+		if s.Label != nil {
+			needToPrintLabel = true
+			break
+		}
+	}
+
+	var b bytes.Buffer
+	w := tabwriter.NewWriter(&b, 0, 0, 1, ' ', tabwriter.Debug)
+	if needToPrintLabel {
+		_, _ = fmt.Fprint(w, "ID\tLabel\tActive\tOutput")
+	} else {
+		_, _ = fmt.Fprint(w, "ID\tActive\tOutput")
+	}
+	for _, s := range states {
+		if needToPrintLabel {
+			if s.Label != nil {
+				_, _ = fmt.Fprintf(w, "\n%d\t%s\t%v\t%v", s.ID, *s.Label, s.Active, string(s.Output))
+			} else {
+				_, _ = fmt.Fprintf(w, "\n%d\t\t%v\t%v", s.ID, s.Active, string(s.Output))
+			}
+		} else {
+			_, _ = fmt.Fprintf(w, "\n%d\t%v\t%v", s.ID, s.Active, string(s.Output))
+		}
+	}
+	_ = w.Flush()
+	return b.String()
+}
+
+func errorNoProcessID(id int) error {
+	return errors.New("no process with id #" + strconv.Itoa(id))
 }
--- responses.go
+++ responses.go
@@ -1,4 +1,4 @@
-package api
+package repository
 
 import "net/http"
 
